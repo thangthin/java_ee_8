021ede9e6f837c8e6e3d5377352ef7205e9b9c63
thangthin <thangthin@gmail.com>
2019-04-14 12:38:38 -0500

    init commit

---
+++ jaxrs_json/build-run-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+mvn clean package
+
+# docker run --rm \
+#  -p 8080:8080 \
+#  -v $(pwd)/target/cars.war:/glassfish5/glassfish/domains/domain1/autodeploy/cars.war \
+#  oracle/glassfish:5.0
+
+
+
+docker build --tag=wildfly-app .
+
+docker run --rm -it -p 8080:8080 wildfly-app
